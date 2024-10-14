938fe15216ff9af5e846096ac94a4574246b0686
Ethan Cao <1677035769@qq.com>
2024-10-14 17:34:18 +0800

    24/10/14 17:34

--- demo.py
+++ demo.py
@@ -1,51 +1,37 @@
-import time
 import random
 
 
-def partition(nums, lI, rI):
-    pivot = nums[lI]
-    i, j = lI + 1, rI
+def COUNTING_SORT(nums, k, map=lambda x: x):
+    out = [0] * len(nums)
+    posSum = [0] * (k + 1)
 
-    while True:
-        # 从左往右 找到大于pivot的元素
-        while nums[i] <= pivot and i < rI:
-            i += 1
-        # 从右往左 找到小于等于pivot的元素
-        while nums[j] > pivot and j > lI:
-            j -= 1
-        if i < j:
-            (nums[i], nums[j]) = (nums[j], nums[i])
-            i, j = i + 1, j - 1
-        else:
-            break
+    # 统计每种元素的个数 Θ(n)
+    for i in range(len(nums)):
+        posSum[map(nums[i])] += 1
 
-    # 此时j指向的是 最后一个 小于等于pivot的元素
-    # 与pivot交换后, 能够保证左边元素都小于等于pivot, 右边元素都大于pivot
-    (nums[lI], nums[j]) = (nums[j], nums[lI])
-    return j
+    # 累加小于等于i的元素个数 Θ(k)
+    for i in range(1, k + 1):
+        posSum[i] += posSum[i - 1]
 
-def rand_partition(nums, lI, rI):
-    # 随机选取主元, 并交换到首位
-    i = random.randint(lI, rI)
-    (nums[lI], nums[i]) = (nums[i], nums[lI])
-    return partition(nums, lI, rI)
+    # 从后往前遍历，保证稳定性 Θ(n)
+    for i in range(len(nums) - 1, -1, -1):
+        # 此时小于等于nums[i]的未分配元素个数
+        leq_num = posSum[map(nums[i])]
+        # nums[i]应放在这些元素的最末尾位置
+        out[leq_num - 1] = nums[i]
+        posSum[map(nums[i])] -= 1
 
-def rand_quicksort(nums, lI, rI):
-    if lI < rI:  # nums[lI,rI].length >= 2
-        posI = partition(nums, lI, rI)
-        rand_quicksort(nums, lI, posI - 1)
-        rand_quicksort(nums, posI + 1, rI)
+    return out
 
 
+def RADIX_SORT(nums, k, d):
+    """k:基数, d:最大位数"""
+    map = lambda x: (x // (k ** i)) % k
+    for i in range(d):
+        nums = COUNTING_SORT(nums, k, map)
+    return nums
 
-start_time = time.time()
-for _ in range(100000):
-    nums = random.sample(range(100000), 100)
-    rand_quicksort(nums, 0, len(nums) - 1)
-end_time = time.time()
 
-
-print("Time cost: ", end_time - start_time)
-
-for i in range(1, len(nums)):
-    assert nums[i] >= nums[i - 1]
+nums = random.sample(range(100), 10)
+nums = RADIX_SORT(nums, 10, 2)
+print(nums)
